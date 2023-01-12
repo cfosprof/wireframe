4edee1d1e43df20593e2a950cf11af502c920a5c
Chris Foster <cfosprof@gmail.com>
2023-01-12 15:38:35 -0800

    message

--- app.js
+++ app.js
@@ -9,8 +9,20 @@
 // document.write(`${userAnswer}'s' are awesome!`);
 
 
+// function getName() {
+//     let userDog = prompt("What is your favorite dog?");
+//     if (userDog) {
+//         document.write(`I love ${userDog} too!`);
+//     } else {
+//         alert(`That's not a valid answer, please reload the page again because I don't want to handle a bunch of conditionals to make this work right now`)
+//     }
+// }
+
 function getName() {
     let userDog = prompt("What is your favorite dog?");
+    while (!userDog) {
+        userDog = prompt("It looks like you left that blank,, do you want to plug your keyboard in and try again?")
+    }
     if (userDog) {
         document.write(`I love ${userDog} too!`);
     } else {
@@ -33,5 +45,21 @@ function getUserAnswer2() {
         document.write(`${userAnswer2}'s' are awesome too!`);
     } else {
         alert(`That's not a valid answer, please reload the page again because I don't want to handle a bunch of conditionals to make this work right now`)
+    }
+}
+
+// function repeatImage() {
+//     let userNumber = prompt("How many times do you want to see griffey on the couch?");
+//     userNumber = parseInt(userNumber)
+//     for (let i = 0; i < userNumber; i++) {
+//         document.write('<img src="images/39B523CB-42C9-4D35-AACE-79C832479756.jpg" alt="Outdoor Dog">')
+//     }
+// }
+
+function repeatImage() {
+    let userNumber = prompt("How many times do you want to see griffey on the couch?");
+    userNumber = parseInt(userNumber)
+    for (let i = 0; i < userNumber && userNumber < 8; i++) {
+        document.write('<img src="images/39B523CB-42C9-4D35-AACE-79C832479756.jpg" alt="Outdoor Dog">')
     }
 }
